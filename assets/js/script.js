ed169c26e0eac243571271cdb9775d7e757da8ff
Max Widlund Stark <maxwidlundstark@gmail.com>
2024-04-24 19:03:00 +0000

    adding JS to create questions and setting index and next question function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,48 @@
-console.log(hello);
+// questions for the quiz
+const questions = [
+    {
+        question: "question 1 goes here",
+        answers: [
+            {text: "asnwer1", correct: false},
+            {text: "asnwer2", correct: true},
+            {text: "asnwer3", correct: false},
+            {text: "asnwer4", correct: false},
+        ]
+    }, 
+    {
+        question: "question 2 goes here",
+        answers: [
+            {text: "asnwer1", correct: false},
+            {text: "asnwer2", correct: false},
+            {text: "asnwer3", correct: true},
+            {text: "asnwer4", correct: false},
+        ]
+    },
+    {
+        question: "question 3 goes here",
+        answers: [
+            {text: "asnwer1", correct: true},
+            {text: "asnwer2", correct: false},
+            {text: "asnwer3", correct: false},
+            {text: "asnwer4", correct: false},
+        ]
+    },
+    {
+        question: "question 4 goes here",
+        answers: [
+            {text: "asnwer1", correct: false},
+            {text: "asnwer2", correct: true},
+            {text: "asnwer3", correct: false},
+            {text: "asnwer4", correct: false},
+        ]
+    },
+    {
+        question: "question 5 goes here",
+        answers: [
+            {text: "asnwer1", correct: false},
+            {text: "asnwer2", correct: false},
+            {text: "asnwer3", correct: false},
+            {text: "asnwer4", correct: true},
+        ]
+    }
+];
